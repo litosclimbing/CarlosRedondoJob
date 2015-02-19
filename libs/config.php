7654f71c7df69cd1138c5ee6bb88d3ef1531493d
litosclimbing <litosclimbing@users.noreply.github.com>
2015-02-19 12:22:17 +0100

    Create config.php

---
+++ libs/config.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "job";
+$settings = array(
+	'oauth_access_token' => '3044706309-B6hMmE6gLPvFageW88yxzgS9y9QP3MHKCspyzqw',
+	'oauth_access_token_secret' => 'TR1nVhTL8MbsRMKLMSLogwMehwwDVexGPgWUWGy5X6Nhg',
+	'consumer_key' => 'carlosredper',
+	'consumer_secret' => '3044706309'
+);
+?>
